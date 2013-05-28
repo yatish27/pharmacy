9ae8ec1ff8001ffb88d9d3612128eaef1604eebe
Yatish Mehta <yatish.mehta@betterlabs.net>
2013-05-28 12:37:33 +0530

    added dynamic filters

--- app/admin/contacts.rb
+++ app/admin/contacts.rb
@@ -28,7 +28,7 @@ ActiveAdmin.register Contact do
     column :living_duration
   end
 
-  filter :age_range, :as => :select, :collection => Contact.select("DISTINCT age_range").map(&:age_range).compact.collect {|o| [o, o]}
-  filter :zip, :as => :select, :collection => Contact.select("DISTINCT zip").map(&:zip).compact.collect {|o| [o , o]}
-  filter :home_owner, :as => :select, :collection => Contact.select("DISTINCT home_owner").map(&:home_owner).compact.collect {|o| [o, o]}
+  filter :age_range, :as => :select, :collection => proc { Contact.select("DISTINCT age_range").map(&:age_range).compact.collect {|o| [o, o]}}
+  filter :zip, :as => :select, :collection => proc{ Contact.select("DISTINCT zip").map(&:zip).compact.collect {|o| [o , o]}}
+  filter :home_owner, :as => :select, :collection => proc { Contact.select("DISTINCT home_owner").map(&:home_owner).compact.collect {|o| [o, o]}}
 end
