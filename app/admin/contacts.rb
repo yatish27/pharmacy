ab5eaad25fa12df02ecc301789b1c7f807c90451
Yatish Mehta <yatish.mehta@betterlabs.net>
2013-05-29 16:05:45 +0530

    Added date range in filter

--- app/admin/contacts.rb
+++ app/admin/contacts.rb
@@ -32,4 +32,5 @@ ActiveAdmin.register Contact do
   filter :age_range, :as => :select, :collection => proc { Contact.select("DISTINCT age_range").map(&:age_range).compact.collect {|o| [o, o]}}
   filter :zip, :as => :select, :collection => proc{ Contact.select("DISTINCT zip").map(&:zip).compact.collect {|o| [o , o]}}
   filter :gender,:as => :select, :collection => Contact.select("DISTINCT gender").map(&:gender).compact.collect {|o| [o , o]}
+  filter :created_at,:as => :date_range
 end
