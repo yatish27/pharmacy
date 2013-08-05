af4975806e95859814b29c33fa735f009f0bde19
Yatish Mehta <yatishmehta@Yatishs-MacBook-Pro.local>
2013-08-05 11:07:32 +0530

    Added assets folder into git

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Pharmacy::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.precompile += %w[active_admin.css active_admin/print.css active_admin.js]
   # Generate digests for assets URLs
   config.assets.digest = true
